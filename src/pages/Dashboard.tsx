75f63629fe0ac99c3b832fed05caf4e6101a0f05
zaniarkarimi <zaniarkarimi@chmail.ir>
2025-10-11 15:05:56 +0330

    Updated some parts of the files

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -90,7 +90,7 @@ const Dashboard = () => {
           </p>
         </div>
 
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
           <Card className="shadow-card transition-all hover:scale-[1.02] hover:shadow-primary">
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
